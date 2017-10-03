1df3a38e4c35ca552dbaf4ef6a105522208d28cb
Jordan Klaers <jordan.klaers@gmail.com>
2017-10-03 17:27:25 -0600

    log2

--- index.js
+++ index.js
@@ -8,5 +8,6 @@ board.on('ready', function(){
   console.log("board on");
   // Create an Led on pin 7 on header P1 (GPIO4) and strobe it on/off
   const led = new five.Led(7);
+  console.log(led);
   led.strobe(500);
 });
