78506e48e0c3eed2f076cf44214e8efc939845bf
Jordan Klaers <jordan.klaers@gmail.com>
2017-10-03 16:59:43 -0600

    changed syntax

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const board = new five.Board({
   io: new raspi()
 });
 
-board.on('ready', () => {
+board.on('ready', function(){
   // Create an Led on pin 7 on header P1 (GPIO4) and strobe it on/off
   const led = new five.Led('P1-7');
   led.strobe(500);
