6ad9079d7b0ad45e01899bdc29c1a73d7f354cc0
Jordan Klaers <jordan.klaers@gmail.com>
2017-10-04 21:23:37 -0600

    stay on

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const board = new five.Board({
 board.on('ready', function(){
   console.log("board onnnnn");
   // Create an Led on pin 7 on header P1 (GPIO4) and strobe it on/off
-  const led = new five.Led("P1-13");
+  const blue = new five.Led("P1-13");
 
-  led.strobe(500);
+  blue.on();
 });
