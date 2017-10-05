dc824681ef5071747107b3d508ee4fe9906e533c
Jordan Klaers <jordan.klaers@gmail.com>
2017-10-04 21:52:57 -0600

    trying individual with red.brightness()

--- index.js
+++ index.js
@@ -7,20 +7,21 @@ const board = new five.Board({
 board.on('ready', function(){
   console.log("board onnnnn");
   // Create an Led on pin 7 on header P1 (GPIO4) and strobe it on/off
-  // const blue = new five.Led("P1-13");
-  // const red = new five.Led("P1-11");
-  // const green = new five.Led("P1-15");
-  // red.on();
+  const blue = new five.Led("P1-12");
+  const red = new five.Led("P1-33");
+  const green = new five.Led("P1-35");
+  red.on();
+  red.brightness(10);
 
-  var anode = new five.Led.RGB({
-   pins: {
-     red: "12",
-     green: "33",
-     blue: "35"
-   },
-   isAnode: true
- });
-
-  anode.on();
-  anode.color({red:0,green:100,blue:100});
+ //  var anode = new five.Led.RGB({
+ //   pins: {
+ //     red: "12",
+ //     green: "33",
+ //     blue: "35"
+ //   },
+ //   isAnode: true
+ // });
+ //
+ //  anode.on();
+ //  anode.color({red:0,green:100,blue:100});
 });
