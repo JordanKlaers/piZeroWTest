ef9d271cd034ec5e3f542b0e0cbd16a45dae980e
Jordan Klaers <jordan.klaers@gmail.com>
2017-10-04 21:33:01 -0600

    testing rgb with led.RGB

--- index.js
+++ index.js
@@ -9,5 +9,18 @@ board.on('ready', function(){
   // Create an Led on pin 7 on header P1 (GPIO4) and strobe it on/off
   const blue = new five.Led("P1-13");
   const red = new five.Led("P1-11");
-  red.on();
+  const green = new five.Led("P1-15");
+  // red.on();
+
+  var anode = new five.Led.RGB({
+   pins: {
+     red: "P1-11",
+     green: "P1-15",
+     blue: "P1-13"
+   },
+   isAnode: true
+ });
+
+  anode.on();
+  anode.color({red:0,green:100,blue:100});
 });
