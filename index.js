6520729702d1c3142802610d27344159aa5bc39d
Jordan Klaers <jordan.klaers@gmail.com>
2017-10-04 21:39:33 -0600

    finding pwm pins

--- index.js
+++ index.js
@@ -8,19 +8,19 @@ board.on('ready', function(){
   console.log("board onnnnn");
   // Create an Led on pin 7 on header P1 (GPIO4) and strobe it on/off
   // const blue = new five.Led("P1-13");
-  const red = new five.Led("P1-12");
+  // const red = new five.Led("P1-11");
   // const green = new five.Led("P1-15");
-  red.on();
+  // red.on();
 
- //  var anode = new five.Led.RGB({
- //   pins: {
- //     red: "P1-11",
- //     green: "P1-15",
- //     blue: "P1-13"
- //   },
- //   isAnode: true
- // });
- //
- //  anode.on();
- //  anode.color({red:0,green:100,blue:100});
+  var anode = new five.Led.RGB({
+   pins: {
+     red: "P1-1",
+     green: "P1-33",
+     blue: "P1-35"
+   },
+   isAnode: true
+ });
+
+  anode.on();
+  anode.color({red:0,green:100,blue:100});
 });
